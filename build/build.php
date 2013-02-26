a2fba45ff5250147576075516990750aef78159d
gsaunier <gael.saunier@open-miage.fr>
2013-02-26 21:53:02 +0100

    build fix

--- build/build.php
+++ build/build.php
@@ -8,12 +8,12 @@ if (!is_dir($temp))
     mkdir($temp);
 $version = file_get_contents("build.version");
 $count = intval(file_get_contents("build.count"));
-$dir = "$temp/lib/OpenM/util/$version" . "_$count";
+$dir = "$temp/lib/OpenM-Book/gui/$version" . "_$count";
 if (is_dir($temp))
     OpenM_Dir::rm($temp);
 mkdir($dir, 0777, true);
 OpenM_Dir::cp("../src", $dir);
-$target_file_name = "OpenM.util_$version" . "_$count" . "_src.zip";
+$target_file_name = "OpenM-Book.gui_$version" . "_$count" . "_src.zip";
 OpenM_Zip::zip($temp, $target_file_name);
 OpenM_Dir::rm($temp);
 file_put_contents("build.count", $count + 1);
