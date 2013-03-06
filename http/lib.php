e35cda51e268737179bd6d8ddf98d2754a9eedf3
gsaunier <gael.saunier@open-miage.fr>
2013-03-06 01:37:12 +0100

    impl package from util 1.0.8

--- http/lib.php
+++ http/lib.php
@@ -1,6 +1,6 @@
 <?php
 
 Import::php("util.pkg.OpenM_Dependencies");
-$dependencies = new OpenM_Dependencies(dirname(__DIR__)."/lib");
-$dependencies->addInClassPath(OpenM_Dependencies::RUN);
+$dependencies = new OpenM_Dependencies(dirname(__DIR__) . "/lib");
+$dependencies->addInClassPath(OpenM_Dependencies::RUN, is_dir(dirname(__DIR__) . "/src"));
 ?>
