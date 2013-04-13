62458f6f4f00816e51580fd909a9879e8fcd3072
Nicolas rouzeaud <nicolas.rouzeaud@open-miage.fr>
2013-04-13 18:08:30 +0200

    dev on JS community (modif display, ajout action )

--- src/OpenM-Book/gui/js/js-community.js
+++ src/OpenM-Book/gui/js/js-community.js
@@ -19,31 +19,35 @@ function select_community_navigation(communityId){
     }
     return false;
 }
-function addCommunityAJAX(){
-    alert("Ajout d'une communauté !");
+
+function addCommunityAJAX(idInput,idCommunity){
+    $("#"+bt_addCommunity).popover("hide");
+    var newName = $("#"+input_NewCommunityName).val()
+    $("#"+bt_addCommunity).hide();
+    alert("La nouvelle communauté : " + newName);
 }
 
 
 //Ajoute une communauté (graphique) à la div ZONE
 function addCommunity(community){
-   $(divId_zone_community).append(community.ToZoneHTML());
+   $(divId_community_container).append(community.ToZoneHTML());
     return false;
 }
 
 //Navige dans une banche de communauté
 function addBranchCommunity(community, communityChilds){
    //on supprime le contenu de la Zone COMmunity
-   $(divId_zone_community).empty();   
+   $(divId_community_container).empty();   
    if (!communityChilds && community.nbAncestor ==0 ){
        //1er communauté. pas de navigation
        $(divId_navigation_div).hide();
        addCommunity(community);
-   }else{
-       
-       if (community.nbChild != 0){
+   }else{       
+       //if (community.nbChild != 0){
            //la navigation       
         $(divId_navigation_div).show();
         $(divId_navigation_community).empty();
+        $(divId_divCommunity_container).show();
         if (community.nbAncestor != 0){
             //on utilise ces propres ancetre
             for (i in community.ancestor){
@@ -55,18 +59,50 @@ function addBranchCommunity(community, communityChilds){
         for (idarray in communityChilds){ 
             addCommunity(communityChilds[idarray]);
         }
-        //if (community.usrCanAddSubCommu){
-           var ajout = "<div class='span3'><a href='#' onclick='addCommunityAJAX();' class='btn btn-inverse btn-large'><i class='icon-white icon-plus'></i>&nbsp;Ajout communauté</a></div>";
-            //<div id='community-"+ this.id +"' class='community span3'
-            $(divId_zone_community).append(ajout);
-        //}                
-       }
-       else{
+        if (community.usrCanAddSubCommu){
+           //le bouton dans les sous communautés
+           
+           var ajout = "<div class='span3'><a href='#' id='"+bt_addCommunity+"' class='btn btn-inverse btn-large'><i class='icon-white icon-plus'></i>&nbsp;Ajout communauté</a></div>";
+            $(divId_community_container).append(ajout);
+            var option={placement:'bottom', title: 'Nouvelle communauté',
+              content: '<div class="control-group"><label class="control-label" for="'+input_NewCommunityName+'">Name</label><div class="controls"><input type="text" class="input-large" id="'+input_NewCommunityName+'" placeholder="Nom ?"></div><a href="#" onclick="addCommunityAJAX()" class="btn btn-primary btn-small"><i class="icon-white icon-ok-circle"></i> Enregistrer</a></div>',
+              html: true
+            };
+            $('#'+bt_addCommunity).popover(option);
+            
+        }else{
+            if (communityChilds.length ==0){
+                $(divId_divCommunity_container).hide();
+            }
+        }        
+          
+          var actionsHTML = community.generateActions();
+          if (actionsHTML){
+              $(divId_action).show();
+              $(divid_action_container).empty()
+                                       .append(actionsHTML);
+              //activation toolTip
+              $("[rel='tooltip']").tooltip();
+              
+              //activation popover             
+              /* var option={title: 'Nouvelle sous communauté',
+                          content: '<div class="control-group"><label class="control-label" for="'+input_NewCommunityName+'">Name</label><div class="controls"><input type="text" class="input-large" id="'+input_NewCommunityName+'" placeholder="Nom ?"></div><a href="#" onclick="addCommunityAJAX()" class="btn btn-primary btn-small"><i class="icon-white icon-ok-circle"></i> Enregistrer</a></div>',
+                          html: true,
+                          placement: 'bottom'};       
+                $('a[id*="'+bt_addCommunity+'"]').popover(option);  
+              */
+              
+          }else{
+             $(divId_action).hide(); 
+          }
+      // }
+       //else{           
            //pas d'enfant donc on laisse la communauté'
            //addCommunity(community);
-           addBranchCommunity(community.lastAncestor,community.lastAncestor.child );
-       }   
+       //    addBranchCommunity(community.lastAncestor,community.lastAncestor.child );
+       // }       
    }
+   return false;
 }
 
  
@@ -137,8 +173,8 @@ function showError(message){
 }
 
 function showLoading(){
-    $(divId_zone_community).empty(); 
-    $(divId_zone_community).html("<img src='"+ressources_dir+"OpenM-Book/gui/img/ajax-loader.gif'>");
+    $(divId_community_container).empty(); 
+    $(divId_community_container).html("<img src='"+ressources_dir+"OpenM-Book/gui/img/ajax-loader.gif'>");
 } 
 
 //Class Community   
@@ -180,12 +216,12 @@ function Community(id, name, usrCanAddSubCommu, usrCanRegisterInto ,url) {
         {
            if (this.usrCanAddSubCommu){
                str="<div id='community-"+ this.id +"' class='community span3' onclick='select_community("+ this.id +");' >"+ this.name +"</div>";         
-           }else{
+           /*}else{
                if (this.nbChild >0){
                    str="<div id='community-"+ this.id +"' class='community span3' onclick='select_community("+ this.id +");' >"+ this.name +"</div>";                    
                }else{
                    str="<div id='community-"+ this.id +"' class='community span3' onclick='alert("+ this.id +");' >"+ this.name +"</div>";                    
-               }
+               }*/
            }           
         }
         return str;
@@ -214,181 +250,39 @@ function Community(id, name, usrCanAddSubCommu, usrCanRegisterInto ,url) {
        }
        return clone;
    }
+   
+   this.generateActions = function(){
+       var boutons = "";
+       var texte ="";
+       var texteToolTip_usrAddInto = "S&apos;enregistrer dans cette communauté";
+       if (this.usrCanRegisterInto){
+          texte = "S'enregistrer";
+          boutons += "<div class='span2'><a href='#' class='btn btn-inverse' rel='tooltip' data-placement='top' data-toggle='tooltip' data-original-title='"+texteToolTip_usrAddInto+"'><i class='icon-white icon-ok'></i>&nbsp;"+texte+"</a></div>";          
+      }
+       /*if (this.usrCanAddSubCommu){
+          texte = "Ajouter une sous communauté";
+          texteToolTip_usrAddSubCommunity = "Ajouter une sous communauté à "+this.name;
+          boutons += "<div class='span2'><a href='#' id='bt_addCommunity"+nb_bt_addCommunity+"' class='btn btn-inverse' rel='tooltip' data-placement='top' data-toggle='tooltip' data-original-title='"+texteToolTip_usrAddSubCommunity+"'><i class='icon-white icon-plus'></i>&nbsp;"+texte+"</a></div>";    
+          nb_bt_addCommunity ++;
+       }*/
+       
+       return boutons;
+   }
+   
 } 
 
 
 /**  Variable Globale   **/
-divId_zone_community = "#zone_community";
+divId_community_container = "#community_container";
+divId_divCommunity_container = "#div_community_container";
 divId_navigation_community = "#navigation_community";    
 divId_navigation_div = "#navigation_div";
 divId_alert = "#div_alert";
-AllCommunities = new Array();  
- 
-   
-   
-   
- /* Jeux de données */  
- /*
-community01 = new Community(1, "Miage", "#"); 
-AllCommunities[community01.id] = community01;
-community02 = new Community(2, "Paris", "#");
-AllCommunities[community02.id] = community02;
-community03 = new Community(3, "Toulouse", "#");
-AllCommunities[community03.id] = community03;
-community04 = new Community(4, "Marseille", "#");
-AllCommunities[community04.id] = community04;
-community05 = new Community(5, "L3", "#");
-AllCommunities[community05.id] = community05;
-community06 = new Community(6, "M1", "#");
-AllCommunities[community06.id] = community06;
-community07 = new Community(7, "M2", "#");
-AllCommunities[community07.id] = community07;
-community08 = new Community(8, "L3", "#");
-AllCommunities[community08.id] = community08;
-community09 = new Community(9, "M1", "#");
-AllCommunities[community09.id] = community09;
-community10 = new Community(10, "2010", "#");
-AllCommunities[community10.id] = community10;
-community11 = new Community(11, "2011", "#")
-AllCommunities[community11.id] = community11;
-community12 = new Community(12, "2012", "#");
-AllCommunities[community12.id] = community12;
-community13 = new Community(13, "2010", "#");
-AllCommunities[community13.id] = community13;
-community14 = new Community(14, "2012", "#");
-AllCommunities[community14.id] = community14;
-community15 = new Community(15, "2009", "#");
-AllCommunities[community15.id] = community15;
-community16 = new Community(16, "2010", "#");
-AllCommunities[community16.id] = community16;
-community17 = new Community(17, "2013", "#");
-AllCommunities[community17.id] = community17;
-community18 = new Community(18, "M1", "#");
-AllCommunities[community18.id] = community18;
-community19 = new Community(19, "2012", "#");
-AllCommunities[community19.id] = community19;
+divId_action = "#action";
+divid_action_container = "#action_container";
+bt_addCommunity = 'bt_addCommunity';
+nb_bt_addCommunity =0;
+input_NewCommunityName = 'inputNameCommunity';
 
 
-
-community20 = new Community(20, "2012", "#");
-AllCommunities[community20.id] = community20;
-community21 = new Community(21, "2008", "#");
-AllCommunities[community21.id] = community21;
-community22 = new Community(22, "2007", "#");
-community23 = new Community(23, "1999", "#");
-community24 = new Community(24, "2013", "#");
-community25 = new Community(25, "2014", "#");
-community26 = new Community(26, "2001", "#");
-community27 = new Community(27, "2002", "#");
-community28 = new Community(28, "2003", "#");
-community29 = new Community(29, "2004", "#");
-community30 = new Community(30, "2005", "#");
-community31 = new Community(31, "2006", "#");
-community32 = new Community(32, "2000", "#");
-community33 = new Community(33, "2015", "#");
-AllCommunities[community22.id] = community22;
-AllCommunities[community23.id] = community23;
-AllCommunities[community24.id] = community24;
-AllCommunities[community25.id] = community25;
-AllCommunities[community26.id] = community26;
-AllCommunities[community27.id] = community27;
-AllCommunities[community28.id] = community28;
-AllCommunities[community29.id] = community29;
-AllCommunities[community30.id] = community30;
-AllCommunities[community31.id] = community31;
-AllCommunities[community32.id] = community32;
-AllCommunities[community33.id] = community33;
-
-community09.addChild(community20);
-community09.addChild(community21);
-community09.addChild(community22);
-community09.addChild(community23);
-community09.addChild(community24);
-community09.addChild(community25);
-community09.addChild(community26);
-community09.addChild(community27);
-community09.addChild(community28);
-community09.addChild(community29);
-community09.addChild(community30);
-community09.addChild(community31);
-community09.addChild(community32);
-community09.addChild(community33);
-
-
-//Les enfants
-//niv 1/
-community01.addChild(community02);
-community01.addChild(community03);
-community01.addChild(community04);
-//niv 2
-community02.addChild(community18);
-community03.addChild(community05);
-community03.addChild(community06);
-community03.addChild(community07);
-community04.addChild(community08);
-community04.addChild(community09);
-//niv 3
-community18.addChild(community19);
-community07.addChild(community13);
-community07.addChild(community14);
-community08.addChild(community10);
-community08.addChild(community11);
-community08.addChild(community12);
-community09.addChild(community15);
-community09.addChild(community16);
-community09.addChild(community17);
-
-//ancetre
-//nivo 2
-community02.addAncestor(community01);
-community03.addAncestor(community01);
-community04.addAncestor(community01);
-//nivo 3
-community18.addAncestor(community01);
-community18.addAncestor(community02);
-community05.addAncestor(community01);
-community05.addAncestor(community03);
-community06.addAncestor(community01);
-community06.addAncestor(community03);
-community07.addAncestor(community01);
-community07.addAncestor(community03);
-community08.addAncestor(community01);
-community08.addAncestor(community04);
-community09.addAncestor(community01);
-community09.addAncestor(community04);
-//niveau 4
-community19.addAncestor(community01);
-community19.addAncestor(community02);
-community19.addAncestor(community18);
-community13.addAncestor(community01);
-community13.addAncestor(community03);
-community13.addAncestor(community07);
-community14.addAncestor(community01);
-community14.addAncestor(community03);
-community14.addAncestor(community07);
-community10.addAncestor(community01);
-community10.addAncestor(community04);
-community10.addAncestor(community08);
-community11.addAncestor(community01);
-community11.addAncestor(community04);
-community11.addAncestor(community08);
-community12.addAncestor(community01);
-community12.addAncestor(community04);
-community12.addAncestor(community08);
-community15.addAncestor(community09);
-community15.addAncestor(community04);
-community15.addAncestor(community01);
-community16.addAncestor(community09);
-community16.addAncestor(community04);
-community16.addAncestor(community01);
-community17.addAncestor(community09);
-community17.addAncestor(community04);
-community17.addAncestor(community01);
-*/
-/* FIN jeux de données */
-
-
-
-
-
-
+AllCommunities = new Array();
--- src/OpenM-Book/gui/tpl/community.tpl
+++ src/OpenM-Book/gui/tpl/community.tpl
@@ -6,7 +6,7 @@
 
         <style type="text/css">
             {literal}
-                /*[class*="span"]{
+              /*  [class*="span"]{
                 border: black solid 1px;
                 background-color: lightgreen;
                 margin-bottom: 10px;
@@ -29,44 +29,56 @@
 
         <div class="container-fluid container-withmenunavigation">
             {include file='include/alert.tpl'}
+            
+            <div class="row-fluid"><div class="span12">Le retour JSON : <br><pre><code id="retourJSON">  </code></pre></div></div>
 
-            <div class="row-fluid">
-                <div class="span9">
-                    <h1>Communauté</h1>
-                    <br/>
-                    <span>Bonjour !</span>
-                    <p id="monNom" contenteditable="true">{$nom} </p>
-                    <br><br>
-                    Le retour JSON : 
-                    <pre id="retourJSON"></pre>
-                </div>
-            </div>
-
-
-            <div class="hero-unit">     
+            <!-- Zone communauté  -->
+            <div class="hero-unit">                
                 <div class="row-fluid">
-                    <div id="navigation_div" class="span12" style="display: none">
-                        navigation communauté<br><br>
+                    <div id="navigation_div" class="span10 well" style="display: none">
+                        <span>Communauté en cours :</span><br><br>
                         <ul id="navigation_community" class="breadcrumb">
                         </ul>
                     </div>
                 </div>
+                <div id="action" class="row-fluid" style="display: none">
+                    <div class="span10 well">
+                        <span>Action Possible :</span>
+                        <div id="action_container" class="row-fluid">
+                        </div>
+                    </div>                
+                </div>
+               
                 <div class="row-fluid">
-                    <div class="span12">
-                        communauté contenu :<br><br>
-                        <div id="zone_community">
-
-                        </div>                    
+                    <div class="span10 well">
+                        <legend>Contenu de la communauté :<span class="name-community"></span></legend>
+                        <div id="div_community_container" class="row-fluid">
+                            <div class="span10">
+                                <p>Les sous-communautées :</p>
+                                <div id="community_container" class="row-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        
+                        <div class="row-fluid">
+                            <div class="span10">
+                                <p>Utilisateur dans la communauté :</p>
+                                <div id="user_community">
+                                    No body :(
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+              
             </div>
-
+            <!-- FIN Zone communauté  -->
+            
+            
             <div class="row-fluid">
                 <div class="span12">
-                    Utilisateur dans la communauté<br><br>
-                    <div id="user_community">
                     
-                    </div>
                 </div>
             </div>
 
