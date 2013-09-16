b7dae50a5150bebb69e1ab275a6446881a61c2bb
gsaunier <gael.saunier@open-miage.fr>
2013-09-16 18:32:34 +0200

    better display for menu

--- src/OpenM-Book/gui/js/menuGUI.js
+++ src/OpenM-Book/gui/js/menuGUI.js
@@ -1,63 +1,40 @@
 OpenM_BookGUI.menu = {};
 
 OpenM_BookGUI.menu.Left = {
-    menuMobileId: '',
     menuId: '',
     userEntry: $(document.createElement('li')),
     userEntryMobile: $(document.createElement('li')),
     communityEntry: $(document.createElement('li')),
     communityEntryMobile: $(document.createElement('li')),
     init: function() {
-        if (this.menuMobileId !== '') {
-            var menuMobile = $("#" + this.menuMobileId);
-            menuMobile.addClass("navbar navbar menunavigation-phone hidden-desktop");
-            var div = $(document.createElement('div')).addClass("navbar-inner");
-            menuMobile.append(div);
-            var a = $(document.createElement('a')).addClass("btn btn-navbar");
-            a.attr('data-toggle', "collapse");
-            a.attr('data-target', "#subMenuMenuMobile");
-            a.append('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
-            div.append(a);
-            div.append('<a href="' + OpenM_BookController.commons.URL.user() + '">'
-                    + OpenM_BookDAO.user.DAO.me.firstName
-                    + '<br>'
-                    + OpenM_BookDAO.user.DAO.me.lastName + '</a>');
-            var div2 = $(document.createElement('div')).addClass("nav-collapse collapse");
-            div2.attr("id", "subMenuMenuMobile");
-            div.append(div2);
-            var ul = $(document.createElement('ul')).addClass("nav");
-            div2.append(ul);
-            ul.append($(document.createElement('li')).addClass("divider-vertical"));
-            ul.append(this.userEntryMobile.append('<a href="' + OpenM_BookController.commons.URL.user() + '">Mon Profil</a>'));
-            ul.append(this.communityEntryMobile.append('<a href="' + OpenM_BookController.commons.URL.community() + '">Communauté</a>'));
-        }
-        if (this.menuId !== '') {
-            var menu = $("#" + this.menuId);
-            menu.addClass("visible-desktop");
-            var div3 = $(document.createElement('div')).addClass("tabbable tabs-left");
-            menu.append(div3);
-            var ul2 = $(document.createElement('ul')).addClass("nav nav-tabs nav-tabs-menunavigation");
-            div3.append(ul2);
-            var li = $(document.createElement('li')).addClass("cadre-nom");
-            ul2.append(li);
-            var firstName = $(document.createElement("h3"));
-            firstName.css("cursor", "pointer")
-                    .append(OpenM_BookDAO.user.DAO.me.firstName)
-                    .click(function() {
-                OpenM_BookController.commons.URL.clickToUser();
-            });
-            var LastName = $(document.createElement("h3"));
-            LastName.css("cursor", "pointer")
-                    .append(OpenM_BookDAO.user.DAO.me.lastName)
-                    .click(function() {
-                OpenM_BookController.commons.URL.clickToUser();
-            });
-            li.append(firstName)
-                    .append(LastName);
-            ul2.append("<hr>");
-            ul2.append(this.userEntry.append('<a href="' + OpenM_BookController.commons.URL.user() + '">Profil</a>'));
-            ul2.append(this.communityEntry.append('<a href="' + OpenM_BookController.commons.URL.community() + '">Communauté</a>'));
-        }
+        var menu = $("#" + this.menuId);
+        menu.css("position", "fixed");
+        menu.css("top", 40).css("bottom", 0);
+        menu.addClass("visible-desktop");
+        menu.css("background-color","white");
+        var div3 = $(document.createElement('div')).addClass("tabbable tabs-left");
+        menu.append(div3);
+        var ul2 = $(document.createElement('ul')).addClass("nav nav-tabs nav-tabs-menunavigation");
+        div3.append(ul2);
+        var li = $(document.createElement('li')).addClass("cadre-nom");
+        ul2.append(li);
+        var firstName = $(document.createElement("h3"));
+        firstName.css("cursor", "pointer")
+                .append(OpenM_BookDAO.user.DAO.me.firstName)
+                .click(function() {
+            OpenM_BookController.commons.URL.clickToUser();
+        });
+        var LastName = $(document.createElement("h3"));
+        LastName.css("cursor", "pointer")
+                .append(OpenM_BookDAO.user.DAO.me.lastName)
+                .click(function() {
+            OpenM_BookController.commons.URL.clickToUser();
+        });
+        li.append(firstName)
+                .append(LastName);
+        ul2.append("<hr>");
+        ul2.append(this.userEntry.append('<a href="' + OpenM_BookController.commons.URL.user() + '">Profil</a>'));
+        ul2.append(this.communityEntry.append('<a href="' + OpenM_BookController.commons.URL.community() + '">Communauté</a>'));
     },
     selectUser: function() {
         this.userEntry.addClass("active");
--- src/OpenM-Book/gui/tpl/include/navBar.tpl
+++ src/OpenM-Book/gui/tpl/include/navBar.tpl
@@ -1,22 +1,16 @@
-<div id="nabBar" class="navbar navbar-inverse">
+<div class="navbar navbar-inverse navbar-fixed-top">
     <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target="#subMenuNavBar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="{$root}#">Open-MIAGE</a>
-            <div id="subMenuNavBar" class="nav-collapse collapse"> 
-                <ul class="nav">
-                    <li class="divider-vertical"></li>
-                    {foreach from=$nav_bar item=menu}<li class="dropdown"><a class="dropdown-toggle"  {if $menu.items} data-toggle="dropdown"{/if} href="{$menu.link}"> {$menu.label} {if $menu.items}<b class="caret"></b>{/if} </a>
-                        {if $menu.items}<ul class="dropdown-menu">
-                            {foreach from=$menu.items item=item}<li><a href="{$item.link}" {if $item.blank}target="_blank"{/if}>{$item.label}</a></li>{/foreach}
-                        </ul>{/if}                        
-                    </li>{/foreach}                    
-                </ul>
-            </div>
-        </div>
+        <ul class="nav">
+            <li>
+                <a id="button-navbar-left" class="btn btn-navbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>  
+            </li>
+            <li class="divider-vertical"></li>
+            <li><a class="brand" class="text-center" href="{$root}#">OpenM-Book</a></li>
+        </ul>
+
     </div>
 </div>
