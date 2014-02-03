d882caa41d6688a7d055d75c262be05a9d96acd1
gsaunier <gael.saunier@open-miage.fr>
2014-02-03 18:04:44 +0100

    externalize OpenM_Book_JSGeneratorServer.class.php to OpenM-Services.gui

--- http/js/index.php
+++ http/js/index.php
@@ -9,8 +9,8 @@ Import::php("OpenM-Services.gui.OpenM_ServiceView");
 $property = Properties::fromFile(OpenM_SERVICE_CONFIG_FILE_NAME);
 OpenM_Log::init(OpenM_SERVICE_CONFIG_DIRECTORY . "/" . $property->get(OpenM_ServiceView::LOG_PATH_PROPERTY), $property->get(OpenM_ServiceView::LOG_LEVEL_PROPERTY), $property->get(OpenM_ServiceView::LOG_FILE_NAME), $property->get(OpenM_ServiceView::LOG_LINE_MAX_SIZE));
 
-Import::php("OpenM-Book.gui.OpenM_Book_JSGeneratorServer");
+Import::php("OpenM-Services.gui.OpenM_JSGeneratorServer");
 Import::php("OpenM-Controller.gui.OpenM_ViewDefaultServer");
-$server = new OpenM_Book_JSGeneratorServer($property->get(OpenM_ViewDefaultServer::ROOT) . "api/", OpenM_SERVICE_CONFIG_DIRECTORY . "/" . $property->get("Smarty.template_c.dir"), OpenM_SERVICE_CONFIG_DIRECTORY . "/" . $property->get("Smarty.cache.dir"));
+$server = new OpenM_JSGeneratorServer($property->get(OpenM_ViewDefaultServer::ROOT) . "api/", OpenM_SERVICE_CONFIG_DIRECTORY . "/" . $property->get("Smarty.template_c.dir"), OpenM_SERVICE_CONFIG_DIRECTORY . "/" . $property->get("Smarty.cache.dir"));
 $server->handle();
 ?>
